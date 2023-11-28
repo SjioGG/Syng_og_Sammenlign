e550fb82d6a371b6ba12445090a7ff78b8053a4e
Theeone <42763916+Theeeone@users.noreply.github.com>
2023-11-28 16:17:55 +0100

    Refactor file paths and add comments

--- control_Unit/Correlation.py
+++ control_Unit/Correlation.py
@@ -3,25 +3,34 @@
 
 import numpy as np
 import scipy.io.wavfile as wav
+import wave
+from pydub import AudioSegment
 
-chosenSong = "Justin_Bieber_-_Ghost"            #Temporary data, this should be a getFunction, for the string that contains the file name
-directoryPath = "this/is/a/path/to/a/file.wav"  #Temporary path to the .wav file.
-#path = directoryPath + chosenSong + ".wav"     #Assuming the file is stored as "songName" + ".wav"
-path1 = chosenSong + ".wav"                      #This is for testing, correct function is above
+chosenSong = "Justin_Bieber_-_Ghost"                    #Temporary data, this should be a getFunction, for the string that contains the file name
+recordedSong = "recording"                              #Temporary data, this should be a getFunction, for the string that contains the file name
+directoryPath = "this/is/a/path/to/a/file.wav"          #Temporary path to the .wav file.
+
+#chosenPath = directoryPath + chosenSong + ".wav"       #Assuming the file is stored as "songName" + ".wav"
+#recordedPath = directoryPath + "recordedSong" + ".wav" #Assuming the file is stored as "recordedSong" + ".wav"
+
+path1 = chosenSong + ".wav"                             #This is for testing, correct function is above
 
 class ControlUnit:
     
     #constructor that takes the path to the wav file as input
-    def __init__(self, path):
-        self.path = path               #path to the file.
-        rate1, data1 = wav.read(path1) #read the sampling rate 'rate1' and the .wav data 'data1' from the file
-        print("Rate:", rate1)          #prints the sampling rate
+    def __init__(self, chosenPath):
+        self.path = cosenPath               #path to the file.
+        rate1, data1 = wav.read(path1)      #read the sampling rate 'rate1' and the .wav data 'data1' from the file
+        print("Rate:", rate1)               #prints the sampling rate
 
 
 
-file1 = "Justin_Bieber_-_Ghost.wav"
+file1 = wave.open("Justin_Bieber_-_Ghost.wav", "rb")
+print("parameters:", file1.getparams())
 
+time_file1 = file1.getnframes() / file1.getframerate()
+print("time:", time_file1)
 
 # Read audio data
-rate1, data1 = wav.read(file1)
-print("Rate:", rate1)
+#rate1, data1 = wav.read(file1)
+#print("Rate:", rate1)
