ee1e8f178e366b607cbf88cade6f32dbd84c7f3a
SjioGG <sdybdahl8@gmail.com>
2023-11-29 23:44:37 +0100

    Tested, ready to integrate with client logic

--- db_server/Server/file_server.cpp
+++ db_server/Server/file_server.cpp
@@ -8,6 +8,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sqlite3.h>
+#include <vector> // For tokenizing the string, example of how to utilize getline()
 
 
 using namespace std;
@@ -171,7 +172,7 @@ public:
         	else if (requestType == "ADD_SCORE")
         	{
             	// Handle the request to update the score table
-            	addScore(additionalInt);
+            	addScoreToDB(additionalInt);
         	}
 			else if (requestType == "SEND_SCORES")
         	{
@@ -219,7 +220,7 @@ public:
     		exit(1);
 		}
 
-    	// Create a string to store the rJane esult
+    	// Create a string to store the result
     	std::string result;
 		std::string instrumental_file;
 		std::string melody_file;
@@ -239,7 +240,7 @@ public:
         	const char *lyricsFile = reinterpret_cast<const char *>(sqlite3_column_text(statement, 7));
 
         	// Construct a string with the fetched data and append it to the result
-        	result += std::to_string(songId) + "|" + artistName + "|" + title + "|" + std::to_string(duration) + "|" + key + "|" + instrumentalFile + "|" + melodyFile + "|" + lyricsFile + "\n";
+        	result += std::to_string(songId) + "|" + artistName + "|" + title + "|" + std::to_string(duration) + "|" + key + "|" + instrumentalFile + "|" + melodyFile + "|" + lyricsFile;
 			
 			// For file transfer (Bring them out of this scope)
 			instrumental_file = instrumentalFile;
@@ -253,6 +254,7 @@ public:
 
     	// Send the combined data to the client
     	send(newSocketDescriptor, result.c_str(), result.size(), 0);
+		sendScores(songId);
 		sleep(1);
 		send(newSocketDescriptor, instrumental_file.c_str(), result.size(), 0);
 		sendFile(instrumental_file);
@@ -265,21 +267,133 @@ public:
     	printf("Sent all data to the client\n");
 	}
 
-    void addScore(int songId)
+    void addScoreToDB(int songId)
     {
-        // Implement logic to update the score table in the database
-        // ... SQL stuff
+    	char newRow[256];
+    	recv(newSocketDescriptor, newRow, sizeof(newRow), 0);
+    	printf("Received request: %s\n", newRow);
 
-        // For now, let's just send a sample response
-        char responseData[256] = "Score updated successfully";
-        send(newSocketDescriptor, responseData, strlen(responseData), 0);
+    	// Create a temporary string from the char array
+    	std::string inputString(newRow);
 
-        printf("Score updated\n");
-    }
+    	// Tokenize the string using strtok
+    	char* token = strtok(newRow, "|");
+    	std::vector<std::string> tokens;
+
+    	while (token != nullptr) {
+        	tokens.push_back(token);
+        	token = strtok(nullptr, "|");
+ 		}
+
+    	// Now tokens vector contains the individual fields
+    	if (tokens.size() == 3) {
+        	// Assuming the format is scoreValue|user|date
+        	int scoreValue = std::stoi(tokens[0]);
+        	std::string user = tokens[1];
+        	std::string date = tokens[2];
+
+        	// Open the SQLite database
+        	sqlite3 *db;
+        	if (sqlite3_open("sos.db", &db) != SQLITE_OK)
+        	{
+            perror("Error: Cannot open the database");
+            exit(1);
+        	}
+
+        	// Prepare the INSERT statement
+        	const char *insertQuery = "INSERT INTO score (song_id, score_value, user, date) VALUES (?, ?, ?, ?)";
+        	sqlite3_stmt *insertStatement;
+        	if (sqlite3_prepare_v2(db, insertQuery, -1, &insertStatement, nullptr) != SQLITE_OK)
+        	{
+            	perror("Error: Cannot prepare the insert query");
+            	sqlite3_close(db);
+            	exit(1);
+        	}
+
+        	// Bind parameters to the insert statement
+    		if (sqlite3_bind_int(insertStatement, 1, songId) != SQLITE_OK ||
+            	sqlite3_bind_int(insertStatement, 2, scoreValue) != SQLITE_OK ||
+            	sqlite3_bind_text(insertStatement, 3, user.c_str(), -1, SQLITE_STATIC) != SQLITE_OK ||
+            	sqlite3_bind_text(insertStatement, 4, date.c_str(), -1, SQLITE_STATIC) != SQLITE_OK)
+        	{
+            	perror("Error: Cannot bind parameters for insert statement");
+            	sqlite3_finalize(insertStatement);
+            	sqlite3_close(db);
+            	exit(1);
+        	}
+
+        	// Execute the insert statement
+        	if (sqlite3_step(insertStatement) != SQLITE_DONE)
+        	{
+            	perror("Error: Cannot execute the insert statement");
+            	sqlite3_finalize(insertStatement);
+            	sqlite3_close(db);
+            	exit(1);
+        	}
+
+        	// Clean up
+        	sqlite3_finalize(insertStatement);
+        	sqlite3_close(db);
+
+        	printf("Added score data for song %d\n", songId);
+    	}
+    	else {
+        	std::cerr << "Invalid input format" << std::endl;
+    	}
+	}
 
 	void sendScores(int songId)
 	{
+    	// Open the SQLite database
+    	sqlite3 *db;
+    	if (sqlite3_open("sos.db", &db) != SQLITE_OK)
+    	{
+        	perror("Error: Cannot open the database");
+        	exit(1);
+    	}
 
+    	// Perform a query to get all data from the 'song' table
+    	const char *query = "SELECT score.id, score.song_id, score.score_value, score.user, score.date FROM score JOIN song ON score.song_id = song.id WHERE song.id = ?";
+    	sqlite3_stmt *statement;
+    	if (sqlite3_prepare_v2(db, query, -1, &statement, nullptr) != SQLITE_OK)
+    	{
+        	perror("Error: Cannot prepare the query");
+        	sqlite3_close(db);
+        	exit(1);
+    	}
+		// Bind the parameter to the statement
+		if (sqlite3_bind_int(statement, 1, songId) != SQLITE_OK)
+		{
+    		perror("Error: Cannot bind parameter");
+    		exit(1);
+		}
+
+    	// Create a string to store the result
+    	std::string result;
+
+    	// Fetch data from the query and append it to the string
+    	while (sqlite3_step(statement) == SQLITE_ROW)
+    	{
+        	// Fetch each column's data
+        	int scoreId = sqlite3_column_int(statement, 0);
+			int songId = sqlite3_column_int(statement, 1);
+			int scoreValue = sqlite3_column_int(statement, 2);
+        	const char *user = reinterpret_cast<const char *>(sqlite3_column_text(statement, 3));
+        	const char *date = reinterpret_cast<const char *>(sqlite3_column_text(statement, 4));
+
+
+        	// Construct a string with the fetched data and append it to the result
+        	result += std::to_string(scoreId) + "|" + to_string(songId) + "|" + to_string(scoreValue) + "|" + user + "|" + date;
+		}
+
+    	// Clean up
+    	sqlite3_finalize(statement);
+    	sqlite3_close(db);
+
+    	// Send the combined data to the client
+    	send(newSocketDescriptor, result.c_str(), result.size(), 0);
+		cout << result << endl;
+    	printf("Sent score data for %d \n", songId);
 	}
 
 };
